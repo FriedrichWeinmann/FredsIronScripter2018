d3f7757e12eee11e6223952ea0c6132d07a65ea9
Friedrich Weinmann <friedrich.weinmann@powershell-stuttgart.de>
2018-01-24 17:05:44 +0100

    Module v1

---
+++ FredsIronScripter2018/functions/2018-01-14-Prequel-01/Get-FISMonitor.ps1
@@ -0,0 +1,167 @@
+﻿function Get-FISMonitor
+{
+<#
+	.SYNOPSIS
+		Gathers information on the target's monitors.
+	
+	.DESCRIPTION
+		This function uses CIM to gather information about the target computer(s)'s monitors.
+	
+	.PARAMETER ComputerName
+		The computer to gather information on.
+	
+	.PARAMETER Credential
+		The credentials to use to gather information.
+		This parameter is ignored for local queries.
+	
+	.PARAMETER Authentication
+		The authentication method to use to gather the information.
+		Uses the system default settings by default.
+		This parameter is ignored for local queries.
+	
+	.PARAMETER CimSession
+		Reuse an already established CimSession.
+	
+	.PARAMETER EnableException
+		This parameters disables user-friendly warnings and enables the throwing of exceptions.
+		This is less user friendly, but allows catching exceptions in calling scripts.
+	
+	.EXAMPLE
+		PS C:\> Get-FISMonitor
+	
+		Returns monitor information on the local computer.
+	
+	.EXAMPLE
+		PS C:\> Get-Content servers.txt | Get-FISMoonitor
+	
+		Returns monitor information on all computers listed in servers.txt
+	
+	.EXAMPLE
+		PS C:\> Get-ADComputer -Filter "name -like 'Desktop*'" | Get-FISMonitor
+	
+		Returns monitor information on all computers in ad whose name starts with "Desktop"
+#>
+	[CmdletBinding(DefaultParameterSetName = 'ComputerName')]
+	Param (
+		[Parameter(ValueFromPipeline = $true, ParameterSetName = 'ComputerName')]
+		[PSFComputer[]]
+		$ComputerName = $env:COMPUTERNAME,
+		
+		[System.Management.Automation.CredentialAttribute()]
+		[System.Management.Automation.PSCredential]
+		$Credential,
+		
+		[Microsoft.Management.Infrastructure.Options.PasswordAuthenticationMechanism]
+		$Authentication = [Microsoft.Management.Infrastructure.Options.PasswordAuthenticationMechanism]::Default,
+		
+		[Parameter(ValueFromPipeline = $true, ParameterSetName = 'Session')]
+		[Microsoft.Management.Infrastructure.CimSession[]]
+		$CimSession,
+		
+		[switch]
+		$EnableException
+	)
+	
+	begin
+	{
+		Write-PSFMessage -Level InternalComment -Message "Bound parameters: $($PSBoundParameters.Keys -join ', ')" -Tag 'debug'
+	}
+	process
+	{
+		#region Process by Computer Name
+		foreach ($Computer in $ComputerName)
+		{
+			Write-PSFMessage -Level VeryVerbose -Message "[$Computer] Establishing connection" -Target $Computer -Tag 'connect', 'start'
+			
+			try
+			{
+				if (-not $Computer.IsLocalhost)
+				{
+					$session = New-CimSession -ComputerName $Computer -Credential $Credential -Authentication $Authentication -ErrorAction Stop
+					$computerSystem = Get-CimInstance -ClassName Win32_ComputerSystem -CimSession $session -ErrorAction Stop
+					$bios = Get-CimInstance -ClassName Win32_Bios -CimSession $session -ErrorAction Stop
+					$monitors = Get-CimInstance -ClassName wmiMonitorID -Namespace root\wmi -CimSession $session -ErrorAction Stop
+				}
+				else
+				{
+					# No point in establishing a session to localhost, custom credentials also not supported
+					$computerSystem = Get-CimInstance -ClassName Win32_ComputerSystem -ErrorAction Stop
+					$bios = Get-CimInstance -ClassName Win32_Bios -ErrorAction Stop
+					$monitors = Get-CimInstance -ClassName wmiMonitorID -Namespace root\wmi -ErrorAction Stop
+				}
+			}
+			catch
+			{
+				if ($_.CategoryInfo.Category -eq "NotImplemented")
+				{
+					Stop-PSFFunction -Message "[$Computer] Failed to execute, 'Not Implemented'. This usually happens when running the command against a server without monitor" -Target $Computer -Tag 'connect', 'fail' -ErrorRecord $_ -EnableException $EnableException -Continue -OverrideExceptionMessage
+				}
+				else
+				{
+					Stop-PSFFunction -Message "[$Computer] Failed to connect to target computer" -Target $Computer -Tag 'connect', 'fail' -ErrorRecord $_ -EnableException $EnableException -Continue
+				}
+			}
+			
+			foreach ($monitor in $monitors)
+			{
+				$object = [PSCustomObject]@{
+					ComputerName  = $computerSystem.Name
+					ComputerType   = $computerSystem.Model
+					ComputerSerial = $bios.SerialNumber
+					MonitorSerial  = ($monitor.SerialNumberID | Where-Object { $_ } | ForEach-Object { [char]$_ }) -join ""
+					MonitorType = ($monitor.UserFriendlyName | Where-Object { $_ } | ForEach-Object { [char]$_ }) -join ""
+				}
+				
+				Write-PSFMessage -Level Verbose -Message "[$Computer] Processing $($object.MonitorType)" -Target $Computer -Tag 'monitor','processing','gathered'
+				$null = $object.PSObject.TypeNames.Insert(0, "Fred.IronScripter2018.Monitor")
+				$object
+			}
+		}
+		#endregion Process by Computer Name
+		
+		#region Process by CimSession
+		foreach ($Session in $CimSession)
+		{
+			$sessionDisplayName = "{0} / {1}" -f $Session.Name, $Session.ComputerName
+			Write-PSFMessage -Level VeryVerbose -Message "[$sessionDisplayName] Retrieving data" -Target $Session -Tag 'connect', 'start'
+			
+			try
+			{
+				$computerSystem = Get-CimInstance -ClassName Win32_ComputerSystem -CimSession $Session -ErrorAction Stop
+				$bios = Get-CimInstance -ClassName Win32_Bios -CimSession $Session -ErrorAction Stop
+				$monitors = Get-CimInstance -ClassName wmiMonitorID -Namespace root\wmi -CimSession $Session -ErrorAction Stop
+			}
+			catch
+			{
+				if ($_.CategoryInfo.Category -eq "NotImplemented")
+				{
+					Stop-PSFFunction -Message "[$sessionDisplayName] Failed to execute, 'Not Implemented'. This usually happens when running the command against a server without monitor" -Target $Session -Tag 'connect', 'fail' -ErrorRecord $_ -EnableException $EnableException -Continue -OverrideExceptionMessage
+				}
+				else
+				{
+					Stop-PSFFunction -Message "[$sessionDisplayName] Failed to gather data from target computer" -Target $Session -Tag 'connect', 'fail' -ErrorRecord $_ -EnableException $EnableException -Continue
+				}
+			}
+			
+			foreach ($monitor in $monitors)
+			{
+				$object = [PSCustomObject]@{
+					ComputerName   = $computerSystem.Name
+					ComputerType   = $computerSystem.Model
+					ComputerSerial = $bios.SerialNumber
+					MonitorSerial  = ($monitor.SerialNumberID | Where-Object { $_ } | ForEach-Object { [char]$_ }) -join ""
+					MonitorType    = ($monitor.UserFriendlyName | Where-Object { $_ } | ForEach-Object { [char]$_ }) -join ""
+				}
+				
+				Write-PSFMessage -Level Verbose -Message "[$sessionDisplayName] Processing $($object.MonitorType)" -Target $Session -Tag 'monitor', 'processing', 'gathered'
+				$null = $object.PSObject.TypeNames.Insert(0, "Fred.IronScripter2018.Monitor")
+				$object
+			}
+		}
+		#endregion Process by CimSession
+	}
+	end
+	{
+	
+	}
+}
---
+++ FredsIronScripter2018/tests/pester.ps1
@@ -0,0 +1,76 @@
+﻿param (
+	$Show = "None"
+)
+
+Write-Host "Starting Tests" -ForegroundColor Green
+Write-Host "Installing Pester" -ForegroundColor Cyan
+if ($env:BUILD_BUILDURI -like "vstfs*")
+{
+    Install-Module Pester -Force -SkipPublisherCheck
+	Install-Module PSFramework -Force -SkipPublisherCheck
+}
+
+Write-PSFMessage -Level Important -Message "Importing Module"
+
+Remove-Module FredsIronScripter2018 -ErrorAction Ignore
+Import-Module "$PSScriptRoot\..\FredsIronScripter2018.psd1"
+Import-Module "$PSScriptRoot\..\FredsIronScripter2018.psm1" -Force
+
+$totalFailed = 0
+$totalRun = 0
+
+$testresults = @()
+
+Write-PSFMessage -Level Important -Message "Modules imported, proceeding with general tests"
+foreach ($file in (Get-ChildItem "$PSScriptRoot\general" -Filter "*.Tests.ps1"))
+{
+	Write-PSFMessage -Level Significant -Message "  Executing <c='em'>$($file.Name)</c>"
+	$results = Invoke-Pester -Script $file.FullName -Show $Show -PassThru
+	foreach ($result in $results)
+	{
+		$totalRun += $result.TotalCount
+		$totalFailed += $result.FailedCount
+		$result.TestResult | Where-Object { -not $_.Passed } | ForEach-Object {
+			$name = $_.Name
+			$testresults += [pscustomobject]@{
+				Describe  = $_.Describe
+				Context   = $_.Context
+				Name	  = "It $name"
+				Result    = $_.Result
+				Message   = $_.FailureMessage
+			}
+		}
+	}
+}
+
+Write-PSFMessage -Level Important -Message "Proceeding with individual tests"
+foreach ($file in (Get-ChildItem "$PSScriptRoot\functions" -Recurse -File -Filter "*Tests..ps1"))
+{
+	Write-PSFMessage -Level Significant -Message "  Executing $($file.Name)"
+	$results = Invoke-Pester -Script $file.FullName -Show None -PassThru
+	foreach ($result in $results)
+	{
+		$totalRun += $result.TotalCount
+		$totalFailed += $result.FailedCount
+		$result.TestResult | Where-Object { -not $_.Passed } | ForEach-Object {
+			$name = $_.Name
+			$testresults += [pscustomobject]@{
+				Describe   = $_.Describe
+				Context    = $_.Context
+				Name	   = "It $name"
+				Result	   = $_.Result
+				Message    = $_.FailureMessage
+			}
+		}
+	}
+}
+
+$testresults | Sort-Object Describe, Context, Name, Result, Message | Format-List
+
+if ($totalFailed -eq 0) { Write-PSFMessage -Level Critical -Message "All <c='em'>$totalRun</c> tests executed without a single failure!" }
+else { Write-PSFMessage -Level Critical -Message "<c='em'>$totalFailed tests</c> out of <c='sub'>$totalRun</c> tests failed!" }
+
+if ($totalFailed -gt 0)
+{
+	throw "$totalFailed / $totalRun tests failed!"
+}
